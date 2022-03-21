7fb7c8f86b729620c780e6b7d394d1fdeaefae3d
= <aasif.ali@knoldus.com>
2022-03-21 10:39:40 +0530

    solved all issues

--- src/main/java/com/temperature/tracker/TemperatureTracker.java
+++ src/main/java/com/temperature/tracker/TemperatureTracker.java
@@ -1,25 +1,24 @@
 package com.temperature.tracker;
-
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.vertx.config.ConfigRetriever;
+import io.vertx.config.ConfigRetrieverOptions;
+import io.vertx.config.ConfigStoreOptions;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
 import io.vertx.core.Promise;
 import io.vertx.core.eventbus.Message;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.mongo.MongoClient;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
-import io.vertx.pgclient.PgConnectOptions;
-import io.vertx.pgclient.PgPool;
-import io.vertx.sqlclient.PoolOptions;
-import io.vertx.sqlclient.Row;
-import io.vertx.sqlclient.RowSet;
-import io.vertx.sqlclient.Tuple;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
-import java.util.UUID;
 
 public class TemperatureTracker extends AbstractVerticle {
     /**
@@ -27,16 +26,6 @@ public class TemperatureTracker extends AbstractVerticle {
      */
     private static final Logger LOGGER =
             LoggerFactory.getLogger(TemperatureTracker.class);
-    /**
-     * gets the value of the specified
-     * environment variable "HTTP_PORT" or default.
-     */
-    private static final int HTTP_PORT = Integer.parseInt(
-            System.getenv().getOrDefault("HTTP_PORT", "8181"));
-    /**
-     * Generating random uuid.
-     */
-    private final String uuid = UUID.randomUUID().toString();
     /**
      * starting value of temperature that will be used.
      */
@@ -48,12 +37,12 @@ public class TemperatureTracker extends AbstractVerticle {
     /**
      * create SimpleDateFormat object to format the date.
      */
-    private final SimpleDateFormat sdf =
+    private final SimpleDateFormat dateFormat =
             new SimpleDateFormat("MMM dd,yyyy HH:mm");
     /**
-     * Create the pool.
+     * create mongodb client to communicate with mongodb.
      */
-    private PgPool pgPool;
+    private MongoClient client;
     /**
      *
      * @param startPromise
@@ -61,33 +50,67 @@ public class TemperatureTracker extends AbstractVerticle {
      */
     @Override
     public void start(final Promise<Void> startPromise) throws Exception {
-//        creating database connection.
-        pgPool = PgPool.pool(vertx, new PgConnectOptions()
-                .setPort(5432)
-                .setUser("aasif")
-                .setDatabase("temperaturedb")
-                .setPassword("aasif@12"), new PoolOptions());
+//      To set the configuration file
+        ConfigStoreOptions defaultConfig = new ConfigStoreOptions()
+                .setType("file")
+                .setFormat("json")
+                .setConfig(new JsonObject()
+                        .put("path", "src/main/resources/config.json"));
+        ConfigRetrieverOptions retrieverOptions = new ConfigRetrieverOptions()
+                .addStore(defaultConfig);
+        ConfigRetriever configRetriever =
+                ConfigRetriever.create(vertx, retrieverOptions);
 //        consuming data from eventBus and
 //        sending to recordTemperature() method to insert into database.
         vertx.eventBus()
                 .consumer("temperature.updates", this::recordTemperature);
-        // This handler will get called every second
+        // This handler will get called every second.
         vertx.setPeriodic(1000, this::updateTemperature);
         // Creating router object
         Router router = Router.router(vertx);
         router.get("/all").handler(this::allData);
         router.get("/last-give-minutes").handler(this::getLastFiveMinutes);
-        router.get("/get/:uuid").handler(this::getDataByUuid);
-        vertx.createHttpServer()
-                .requestHandler(router)
-                .listen(HTTP_PORT)
-                .onSuccess(ok -> {
-                    LOGGER.info(
-                            "http server is running: http://127.0.0.1:{}",
-                            HTTP_PORT);
-                    startPromise.complete();
-                })
-                .onFailure(startPromise::fail);
+        router.get("/get/:id").handler(this::getDataById);
+        Handler<AsyncResult<JsonObject>> handler = asyncResult ->
+                this.handleConigResult(asyncResult, router, startPromise);
+        //  To retrieve the values from config.json file
+        configRetriever.getConfig(handler);
+    }
+    /**
+     * This method will retrieve the values from config.json file
+     * and create the server.
+     * @param asyncResult
+     * @param router
+     * @param startPromise
+     */
+    private void handleConigResult(final AsyncResult<JsonObject> asyncResult,
+            final Router router, final Promise<Void> startPromise) {
+        if (asyncResult.succeeded()) {
+            JsonObject config = asyncResult.result();
+            JsonObject http = config.getJsonObject("http");
+            final int httpPort = http.getInteger("port");
+            JsonObject db = config.getJsonObject("db");
+            String dbUrl = db.getString("db_url");
+            String dbName = db.getString("db_name");
+//         ****************** Mongodb connection ****************
+            client = MongoClient.create(vertx, new JsonObject()
+                    .put("url", dbUrl)
+                    .put("db_name", dbName));
+//            creating server
+            vertx.createHttpServer()
+                    .requestHandler(router)
+                    .listen(httpPort)
+                    .onSuccess(ok -> {
+                        LOGGER.info(
+                                "http server is running: http://127.0.0.1:{}",
+                                httpPort);
+                        startPromise.complete();
+                    })
+                    .onFailure(startPromise::fail);
+        } else {
+            LOGGER.info(asyncResult.cause().getMessage());
+            System.out.println("Not red" + asyncResult.cause().getMessage());
+        }
     }
     /**
      * Getting last five minutes data.
@@ -96,34 +119,35 @@ public class TemperatureTracker extends AbstractVerticle {
     private void getLastFiveMinutes(final RoutingContext context) {
         LOGGER.info("Processing http request from {} ",
                 context.request().remoteAddress());
-        Long lastFive = System.currentTimeMillis() - (5 * 60 * 1000);
-        String query =
-                "select * from temperature_records where tstamp >= '"
-                        + lastFive + "'";
         JsonArray data = new JsonArray();
-        pgPool.preparedQuery(query)
-                .execute(ar -> {
-                    if (ar.succeeded()) {
-                        RowSet<Row> rows = ar.result();
-                        for (Row row: rows) {
-                            Long milliSeconds = row.getLong("tstamp");
-                            //creating Date from millisecond
-                            Date date = new Date(milliSeconds);
-                            data.add(new JsonObject()
-                                    .put("uuid", row.getString("uuid"))
-                                    .put("temperature", row.getDouble("value"))
-                                    .put("DateTime", sdf.format(date))
-                            );
-                        }
-                        context.response()
-                                .putHeader("Content-Type", "application/json")
-                                .end(data.encode())
-                                .onFailure(error -> {
-                                    context.fail(500);
-                                    LOGGER.info("Woops", error);
-                                });
-                    }
-                });
+        Long lastFive = System.currentTimeMillis() - (5 * 60 * 1000);
+        JsonObject lastFiveMinutesQuery = new JsonObject()
+                .put("timestamp", new JsonObject().put("$gte", lastFive));
+        client.find("temperature_records", lastFiveMinutesQuery, res -> {
+            if (res.succeeded()) {
+                for (JsonObject json : res.result()) {
+                    Long milliSeconds = json.getLong("timestamp");
+                    //creating Date from millisecond
+                    Date date = new Date(milliSeconds);
+                    data.add(new JsonObject()
+                                    .put("id", json.getString("_id"))
+                                    .put("temperature",
+                                            json.getDouble("temperature"))
+                                    .put("DateTime", dateFormat.format(date))
+                    );
+                }
+            } else {
+                res.cause().printStackTrace();
+            }
+            context.response()
+                    .putHeader("Content-Type", "application/json")
+                    .end(data.encode())
+                    .onFailure(error -> {
+                        context.fail(
+                                HttpResponseStatus.INTERNAL_SERVER_ERROR.code());
+                        LOGGER.info("Woops", error);
+                    });
+        });
     }
     /**
      * Inserting data into database.
@@ -131,20 +155,17 @@ public class TemperatureTracker extends AbstractVerticle {
      */
     private void recordTemperature(
             final Message<JsonObject> jsonObjectMessage) {
-         JsonObject body = jsonObjectMessage.body();
         long timestamp = System.currentTimeMillis();
-        String query =
-                "insert into temperature_records("
-                        + "uuid, value, tstamp) values ($1,$2,$3)";
-        String uuid = body.getString("uuid");
-        double temperature = body.getDouble("temperature");
-        Tuple tuple = Tuple.of(uuid + timestamp, temperature, timestamp);
-        pgPool.preparedQuery(query)
-                .execute(tuple)
-                .onSuccess(rows ->
-                        LOGGER.info("Recorded " + tuple.deepToString()))
-                .onFailure(failure ->
-                        LOGGER.error("Recording failed " + failure));
+        client.insert("temperature_records", new JsonObject()
+                .put("temperature", temperature)
+                .put("timestamp", timestamp), handler -> {
+            if (handler.succeeded()) {
+                LOGGER.info("Recorded " + handler.result());
+            } else {
+                LOGGER.error("Recording failed ");
+                handler.cause().printStackTrace();
+            }
+        });
     }
     /**
      * Getting all the data.
@@ -153,68 +174,72 @@ public class TemperatureTracker extends AbstractVerticle {
     private void allData(final RoutingContext context) {
         LOGGER.info("Processing all data from {} ",
                 context.request().remoteAddress());
-        String query = "select * from temperature_records";
-        pgPool.preparedQuery(query)
-                .execute()
-                .onSuccess(rows -> {
-                    JsonArray jsonArray = new JsonArray();
-                    for (Row row:rows) {
-                        Long milliSeconds = row.getLong("tstamp");
+        JsonObject query = new JsonObject()
+                .put("timestamp", new JsonObject().put("$gt", 1000));
+        JsonArray data = new JsonArray();
+        client.find("temperature_records", query, res -> {
+            if (res.succeeded()) {
+                try {
+                    for (JsonObject json : res.result()) {
+                        Long milliSeconds = json.getLong("timestamp");
                         //creating Date from millisecond
                         Date date = new Date(milliSeconds);
-                        jsonArray.add(new JsonObject()
-                                .put("uuid", row.getString("uuid"))
-                                .put("temp", Math.
-                                        round(row.
-                                                getDouble("value")
-                                                * 100.0) / 100.0)
-                                .put("timestamp", sdf.format(date)));
+                        data.add(new JsonObject()
+                                .put("id", json.getString("_id"))
+                                .put("temperature",
+                                        json.getDouble("temperature"))
+                                .put("DateTime", dateFormat.format(date))
+                        );
                     }
-                    context.response()
-                            .putHeader("Content-Type", "application/json")
-                            .end(new JsonObject()
-                                    .put("data", jsonArray).encode());
-                })
-                .onFailure(failure -> {
-                    LOGGER.info("Woops", failure);
-                    context.fail(500);
-                });
+                } catch (Exception ex) {
+                    LOGGER.info("Exception: " + ex.getMessage());
+                    ex.printStackTrace();
+                }
+                context.response()
+                        .putHeader("Content-Type", "application/json")
+                        .end(new JsonObject()
+                                .put("data", data).encode());
+            } else {
+                LOGGER.info("Woops");
+                context.fail(500);
+            }
+        });
     }
     /**
-     * Getting data by uuid.
+     * Getting data by id.
      * @param context
      */
-        private void getDataByUuid(final RoutingContext context) {
+        private void getDataById(final RoutingContext context) {
         LOGGER.info("Processing http request from {} ",
                 context.request().remoteAddress());
-        String query = "select * from temperature_records where uuid = $1";
-        String uuid = context.request().getParam("uuid");
+        String id = context.request().getParam("id");
 //        JsonObject payload = createPayload();
         JsonArray data = new JsonArray();
-        pgPool.preparedQuery(query)
-                .execute(Tuple.of(uuid), ar -> {
-                    if (ar.succeeded()) {
-                        RowSet<Row> rows = ar.result();
-                        for (Row row: rows) {
-                            Long milliSeconds = row.getLong("tstamp");
-                            //creating Date from millisecond
-                            Date date = new Date(milliSeconds);
-                            System.out.println(row.getDouble("value"));
-                            data.add(new JsonObject()
-                                    .put("uuid", row.getString("uuid"))
-                                    .put("temperature", row.getDouble("value"))
-                                    .put("timestamp", sdf.format(date))
-                            );
-                        }
-                        context.response()
-                                .putHeader("Content-Type", "application/json")
-                                .end(data.encode())
-                                .onFailure(error -> {
-                                    context.fail(500);
-                                    LOGGER.info("Woops", error);
-                                });
+        JsonObject findQuery = new JsonObject()
+                    .put("_id", id);
+            client.find("temperature_records", findQuery, res -> {
+                if (res.succeeded()) {
+                    for (JsonObject json : res.result()) {
+                        Long milliSeconds = json.getLong("timestamp");
+                        //creating Date from millisecond
+                        Date date = new Date(milliSeconds);
+                        data.add(new JsonObject()
+                                .put("id", json.getString("_id"))
+                                .put("temperature",
+                                        json.getDouble("temperature"))
+                                .put("DateTime", dateFormat.format(date)));
                     }
-                });
+                } else {
+                    res.cause().printStackTrace();
+                }
+                context.response()
+                        .putHeader("Content-Type", "application/json")
+                        .end(data.encode())
+                        .onFailure(error -> {
+                            context.fail(500);
+                            LOGGER.info("Woops", error);
+                        });
+            });
     }
     /**
      *  creating payload.
@@ -222,7 +247,6 @@ public class TemperatureTracker extends AbstractVerticle {
      */
     private JsonObject createPayload() {
         return new JsonObject()
-                .put("uuid", uuid)
                 .put("temperature", temperature)
                 .put("timestamp", System.currentTimeMillis());
     }
